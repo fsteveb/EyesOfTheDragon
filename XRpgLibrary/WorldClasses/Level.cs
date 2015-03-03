eb9ffbe02d58cca4ed3c10e93e09c5598d07a630
Steve Brailsford <fsteveb@gmail.com>
2015-03-03 07:52:58 -0500

    Add Level Class

---
+++ XRpgLibrary/WorldClasses/Level.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using XRpgLibrary.TileEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XRpgLibrary.WorldClasses
+{
+    public class Level
+    {
+        #region Field Region
+        readonly TileMap map;
+        #endregion
+
+        #region Property Region
+        public TileMap Map
+        {
+            get { return map; }
+        }
+        #endregion
+
+        #region Constructor Region
+        public Level(TileMap tileMap)
+        {
+            map = tileMap;
+        }
+        #endregion
+
+        #region Method Region
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spiteBatch, Camera camera)
+        {
+            map.Draw(spiteBatch, camera);
+        }
+        #endregion
+    }
+}
